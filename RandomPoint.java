7d4b26630869b22bb6dcd22e40d901bccd4e9583
frisk99 <81175590+frisk99@users.noreply.github.com>
2022-06-14 22:18:21 +0800

    0610
    20220610

---
+++ RandomPoint.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class RandomPoint {
+	    double r, x, y;
+	    Random random = new Random();
+	    public RandomPoint(double _r, double _x, double _y) {
+	        r = _r; x = _x; y = _y;
+	    }
+	    public double[] randPoint() {
+	        double len = Math.sqrt(random.nextDouble(r * r)), ang = random.nextDouble(2 * Math.PI);
+	        double nx = x + len * Math.cos(ang), ny = y + len * Math.sin(ang);
+	        return new double[]{nx, ny};
+	    }	
+}
